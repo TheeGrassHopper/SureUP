99c0745a0368aa19fcb1529e8c2a87fcd1b8595a
TheeGrassHopper <almortada88@gmail.com>
2015-09-17 17:45:45 -0700

    render json

--- app/controllers/makes_controller.rb
+++ app/controllers/makes_controller.rb
@@ -3,21 +3,28 @@ class MakesController < ApplicationController
   def index
     if session[:condition] == "ALL"
       @makes = Edmunds::Make.new.find_all
+      render json: @makes
     elsif session[:condition] == "USED"
       @makes = Edmunds::Make.new.find_used_makes
+      render json: @makes
     else
       @makes = Edmunds::Make.new.find_new_makes
+      render json: @makes
     end
   end
 
   def show
     @make = params[:name]
+    render json: @make
     if session[:condition] == "ALL"
       @models = Edmunds::Model.new.find_by_make_id(params[:id])
+      render json: @models
     elsif session[:condition] == "USED"
       @models = Edmunds::Model.new.find_used_models_by_make_id(params[:id])
+      render json: @models
     else
       @models = Edmunds::Model.new.find_new_models_by_make_id(params[:id])
+      
     end
   end
 
--- app/controllers/model_years_controller.rb
+++ app/controllers/model_years_controller.rb
@@ -1,30 +1,37 @@
 class ModelYearsController < ApplicationController
-
+  
   def index
     @make = params["make"]
     @model = params["model"]
     if session[:condition] == "ALL"
       @model_years = Edmunds::ModelYear.new.for_model_id(params[:id])
+      render json: @model_years
     elsif session[:condition] == "USED"
       @model_years = Edmunds::ModelYear.new.find_used_model_years_by_model_id(params[:id])
+      render json: @model_years
     else
       @model_years = Edmunds::ModelYear.new.find_new_model_years_by_model_id(params[:id])
+      render json: @model_years
     end
   end
 
   def show
     @styles = Edmunds::Style.new.find_styles_by_model_year_id(params[:id])
+    render json: @styles
     @model_id = @styles.first["modelId"]
     @make_id = @styles.first["makeId"]
     @year = @styles.first["year"]
     @make = @styles.first["makeName"]
     @model = @styles.first["modelName"]
+
   end
 
   def details
     @style_id = params[:id]
     @style = Edmunds::Style.new.find_by_id(@style_id)
+    render json: @style
     @engine = Edmunds::Equipment.new.find_engines_by_style_id(@style_id)
+    render json: @engine
     @model_id = @style.first["modelId"]
     @make_id = @style.first["makeId"]
     @year = @style.first["year"]
@@ -34,5 +41,6 @@ class ModelYearsController < ApplicationController
     @style_name = @style.first["attributeGroups"]["MAIN"]["attributes"]["NAME"]["value"]
     @images = Edmunds::Photo.new.find_by_style_id(@style_id)
   end
+
 
 end
